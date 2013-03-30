75edc9e26d3849ea0378c33d108044dc582298c1
Leo Mejia <kolopaisa@msn.com>
2013-03-30 13:49:10 -0400

    Test changes

--- js/main.js
+++ js/main.js
@@ -7,3 +7,15 @@ Team Members:
     Leo Mejia <kolopaisa@gmail.com>
     David Killeffer <rayden7@gmail.com>
    */
+
+/* use this to test out your function */
+window.onload = function() {
+    // changeColor();
+}
+
+/* changeColor takes a path ID and a color (hex value)
+ and changes that path's fill color */
+function createSVG(id) {
+    document.getElementById(id).style.fill = color;
+
+}
